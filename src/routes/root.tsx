1928a2ce279a52a6985b1875bb1a21a81f684271
aegan123 <juhani@fmail.co.uk>
2022-11-17 21:23:34 +0200

    Add navbar

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,12 +2,50 @@
 Copyright Juhani Vähä-Mäkilä (juhani@fmail.co.uk) 2022.
 Licenced under EUROPEAN UNION PUBLIC LICENCE v. 1.2.
  */
-import React from "react";
+import React, {useState} from "react";
 import {Outlet} from "react-router-dom";
+import Navbar from "../components/NavBar";
+
+// Source: Bulma css documentation
+function toggleNavBurgerEvents() {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            if ($target !== null) {
+                $target.classList.toggle('is-active');
+            }
+        })
+    });
+}
 
 export default function Root() {
+    function getLoginStatus() {
+        // TODO check auth cookie and set value
+        return false
+    }
+
+    function getAdminStatus() {
+        // TODO check if logged in user is admin
+        return true
+    }
+    const showLogin = getLoginStatus()
+    const showAdmin = getAdminStatus()
+
+    document.addEventListener('DOMContentLoaded', toggleNavBurgerEvents);
+
     return (
         <>
+            <Navbar showAdminStatus={showAdmin} showLoginStatus={showLogin}/>
             <div className="columns is-gapless is-8">
                 {/* left column */}
                 <div className="column is-desktop">
