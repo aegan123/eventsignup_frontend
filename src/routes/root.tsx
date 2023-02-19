be4d0a9957ff513f7df9ca412b280706db3f7754
Ville Piili <ville.piili@solita.fi>
2023-02-19 16:12:48 +0200

    Initial commit of unified styles
    Boldy strip headerComponent from jasenrekisteri-frontent

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -4,31 +4,7 @@ Licenced under EUROPEAN UNION PUBLIC LICENCE v. 1.2.
  */
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from '../components/NavBar'
-
-// Source: Bulma css documentation
-function toggleNavBurgerEvents() {
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll('.navbar-burger'),
-    0
-  )
-
-  // Add a click event on each of them
-  $navbarBurgers.forEach((el: HTMLElement) => {
-    el.addEventListener('click', () => {
-      // Get the target from the "data-target" attribute
-      const target = el.dataset.target
-      const $target = target ? document.getElementById(target) : null
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      el.classList.toggle('is-active')
-      if ($target !== null) {
-        $target.classList.toggle('is-active')
-      }
-    })
-  })
-}
+import { HeaderComponent } from '../components/HeaderComponent'
 
 export default function Root() {
   function getLoginStatus() {
@@ -43,21 +19,14 @@ export default function Root() {
   const showLogin = getLoginStatus()
   const showAdmin = getAdminStatus()
 
-  document.addEventListener('DOMContentLoaded', toggleNavBurgerEvents)
-
   return (
-    <>
-      <Navbar showAdminStatus={showAdmin} showLoginStatus={showLogin} />
-      <div className="columns is-gapless is-8">
-        {/* left column */}
-        <div className="column is-desktop"></div>
-        {/* center column */}
-        <div className="column is-8">
+    <div>
+      <div>
+        <HeaderComponent showLogin={showLogin} showAdmin={showAdmin} />
+        <div className="container">
           <Outlet />
         </div>
-        {/* right column */}
-        <div className="column is-2 is-desktop"></div>
       </div>
-    </>
+    </div>
   )
 }
