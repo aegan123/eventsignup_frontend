4dbb63c3af961dc37844ac1959cbd1e449784ccb
Ville Piili <ville.piili@solita.fi>
2023-03-15 13:34:08 +0200

    Format code

--- src/components/Utilities.ts
+++ src/components/Utilities.ts
@@ -2,26 +2,32 @@
 Copyright Juhani Vähä-Mäkilä (juhani@fmail.co.uk) 2022.
 Licenced under EUROPEAN UNION PUBLIC LICENCE v. 1.2.
  */
-import {LocalDateTime, ZoneId} from "@js-joda/core";
+import { LocalDateTime, ZoneId } from '@js-joda/core'
 
-export const serverAddress = "http://localhost:8080"
+export const serverAddress = 'http://localhost:8080'
 
 export function postEvent(path: string, data: any): Promise<Response> {
-    return fetch(serverAddress + path, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {'Content-Type': 'application/json; charset=UTF-8'}
-    })
+  return fetch(serverAddress + path, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+  })
 }
 
-export function postImageData(path: string, fileToUpload: Blob): Promise<Response> {
-    return fetch(serverAddress + path, {
-        method: "POST",
-        body: fileToUpload,
-        headers: {"Content-Type": fileToUpload.type},
-    })
+export function postImageData(
+  path: string,
+  fileToUpload: Blob
+): Promise<Response> {
+  return fetch(serverAddress + path, {
+    method: 'POST',
+    body: fileToUpload,
+    headers: { 'Content-Type': fileToUpload.type },
+  })
 }
 
 export function convertLocalDateToUTCISOString(inputDate: string): string {
-    return LocalDateTime.parse(inputDate).atZone(ZoneId.SYSTEM).withZoneSameInstant(ZoneId.UTC).toString()
+  return LocalDateTime.parse(inputDate)
+    .atZone(ZoneId.SYSTEM)
+    .withZoneSameInstant(ZoneId.UTC)
+    .toString()
 }
--- src/types/Quota.ts
+++ src/types/Quota.ts
@@ -3,6 +3,6 @@ Copyright Juhani Vähä-Mäkilä (juhani@fmail.co.uk) 2023.
 Licenced under EUROPEAN UNION PUBLIC LICENCE v. 1.2.
  */
 export type Quota = {
-    group: string
-    quota: string
+  group: string
+  quota: string
 }
